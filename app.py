8e1f20d659ed28f990f8956608f24a35a5c98bc1
WorkSmartMadagascar <dryprocess@mklenmada.com>
2026-06-16 15:28:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -99,10 +99,8 @@ def get_presence():
     conn.close()
     return jsonify(data)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-    # -----Ajouter à la ffin
-    if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
